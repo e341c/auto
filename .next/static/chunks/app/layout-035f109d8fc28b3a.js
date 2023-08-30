83f058b8d80246d4fdfd717f439e5a6817e26a22
e341c <tabyserik@gmail.com>
2023-08-30 17:32:09 +0600

    edited title

---
+++ .next/static/chunks/app/layout-035f109d8fc28b3a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5315:function(n,e,t){Promise.resolve().then(t.t.bind(t,6685,23)),Promise.resolve().then(t.t.bind(t,810,23)),Promise.resolve().then(t.t.bind(t,2471,23))},810:function(){},2471:function(){}},function(n){n.O(0,[129,685,971,596,744],function(){return n(n.s=5315)}),_N_E=n.O()}]);

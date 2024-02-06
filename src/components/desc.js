1cada15e9e04de89593d58710ed66d055b18dcf2
e341c <tabyserik@gmail.com>
2024-02-06 21:40:33 +0600

    removed whatsapp button^ changed phone number

--- src/components/desc.js
+++ src/components/desc.js
@@ -8,18 +8,18 @@ export default function Desc() {
                     ПРИКУРИТЬ АВТО В ГОРОДЕ АЛМАТЫ 24/7 <br /> ЦЕНА ДОГОВОРНАЯ <br />
                 </h1>
                 <hr className="w-100 hr-gold" />
-                <Link href={"tel:+77071164588"} className="text-decoration-none">
+                <Link href={"tel:+77070178901"} className="text-decoration-none">
                     <h1 className="display-2 text-center text-light mb-5" style={{fontWeight: "400"}}>
-                        +7 707 116 45 88
+                        +7 707 017 89 01
                     </h1>
                 </Link>
-                <div className="w-100 mb-5">
+                {/* <div className="w-100 mb-5">
                     <div className="mb-4">
                         <Link href={"https://api.whatsapp.com/send?phone=77071164588"} className="btn btn-outline-warning m-0 w-100">
                             <strong>WhatsApp</strong>
                         </Link>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     );
